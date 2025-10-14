9f937ac8953ac2c857017df16a240288321ba564
Luiza-CLopes <luizacaissutti@gmail.com>
2025-10-13 21:04:19 -0300

    feature: selection as one championship

--- lib/genetic.h
+++ lib/genetic.h
@@ -1,19 +1,20 @@
 #ifndef _GENETIC_H_
 #define _GENETIC_H_
 
-
 #include "point.h"
-
+#include <vector>
+using std::vector;
 
 // CONSTANTS
-const uint8_t AMOUNT_POINTS = 8;                // 8 or more
-const int POPULATION = 200;                     // amount of AI agents
-const uint8_t STAGNATE_GENERATIONS_MAX = 20;    // amount of sequential stagnate generations to stop the training
-const uint8_t CHANCE_OF_MUTATION = 5;           // chance to mutate, in %
-
+const uint8_t AMOUNT_POINTS = 8;             // 8 or more
+const int POPULATION = 200;                  // amount of AI agents
+const uint8_t STAGNATE_GENERATIONS_MAX = 20; // amount of sequential stagnate generations to stop the training
+const uint8_t CHANCE_OF_MUTATION = 5;        // chance to mutate, in %
+const uint8_t CHAMPIONSHIP_SIZE = 3;
 
 // AI AGENT
-class ai_agent{
+class ai_agent
+{
 public:
     // ATTRIBUTES
     Point genes[AMOUNT_POINTS];
@@ -23,19 +24,52 @@ public:
     // METHODS
 };
 
-
 // FUNCTIONS
 // distributing the points in the cartesian plane
 inline void distributing();
 
 // how to choose the parents for the next generation
-inline void selection()
+inline ai_agent selection(vector<ai_agent> &population)
 {
     // chosen method: Championship
+    ai_agent best;
+    double bestFitness = 1e9; // algo bem grande
+
+    for (int i = 0; i < CHAMPIONSHIP_SIZE; i++)
+    {
+        int idx = rand() % population.size(); // escolhe indivíduo aleatório
+        double fit = evaluation(population[idx].genes);
+
+        if (fit < bestFitness)
+        {
+            bestFitness = fit;
+            best = population[idx];
+        }
+    }
+
+    return best; // returns the best of championship
 }
 
 // how to mutate the child if it hit the chance of mutating
-inline void mutate();
+inline void mutate(ai_agent &agent)
+{
+    // chosen method: Swap Mutation
+    int chance = rand() % 100; // random number from 0 to 99
+
+    if (chance < CHANCE_OF_MUTATION)
+    {
+        // choose index from 0 to 7 randomly and swap with the next index
+        int i = rand() % AMOUNT_POINTS;
+
+        // circular motion to get next index
+        int j = (i + 1) % AMOUNT_POINTS;
+
+        // swap adjacent points
+        Point temp = agent.genes[i];
+        agent.genes[i] = agent.genes[j];
+        agent.genes[j] = temp;
+    }
+}
 
 // how to cross the genes of the two parents to create a new child agent
 inline ai_agent crossing(ai_agent Mother, ai_agent Father)
@@ -57,8 +91,8 @@ inline double evaluation(Point genes[AMOUNT_POINTS])
     double sum = 0;
     int i;
 
-    for(i=0; i<AMOUNT_POINTS-1;i++)
-        sum += genes[i].d2op(genes[i+1]);
+    for (i = 0; i < AMOUNT_POINTS - 1; i++)
+        sum += genes[i].d2op(genes[i + 1]);
 
     return sum;
 }
